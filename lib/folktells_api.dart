f3d60f2da626cfe4571b35c40659582a2a7ebf5a
sowens-csd <stephen.owens@thoughtwire.com>
2022-07-12 09:52:13 -0400

    rf: VersionedItem moved to API

--- lib/folktells_api.dart
+++ lib/folktells_api.dart
@@ -5,3 +5,4 @@ export 'user/online_user_id.dart';
 export 'user/online_user_response.dart';
 export 'user/shared_share_group.dart';
 export 'user/share_group_member.dart';
+export 'util/versioned_item.dart';
